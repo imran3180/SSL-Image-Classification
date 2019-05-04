23df310e82ac442f7be6ca3673784b4e34c14408
dikshameghwal <dikshameghwal@gmail.com>
2019-05-03 20:07:57 -0400

    fixed PCA and changed vae net layers

--- main.py
+++ main.py
@@ -16,8 +16,12 @@ import time
 import pdb
 import numpy as np
 matplotlib.use('Agg')
+import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from torchsummary import summary
 
 # creating folders
 if not os.path.isdir("runs"):
@@ -47,38 +51,91 @@ transformations_names = sorted(name for name in data_transformations.__dict__
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+pca = PCA(.99)
+
 def loss_function(x_hat, x, mu, logvar):
-    BCE = nn.functional.binary_cross_entropy(
-        x_hat, x.view(-1, 1024), reduction='sum'
-    )
+    # BCE = nn.functional.binary_cross_entropy(
+    #     x_hat, x, reduction='sum'
+    # )
+    loss = nn.MSELoss()
+    reconstruction_loss = loss(x, x_hat)
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-    return BCE + KLD
+    return reconstruction_loss + KLD
 
 current_time = str(datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S"))
 file = open("runs/run-" + current_time, "w")
 
-def plot_grid(fig, plot_input, actual_output, row_no):
-    grid = ImageGrid(fig, 121, nrows_ncols=(row_no, 2), axes_pad=0.05, label_mode="1")
-    actual_output = actual_output.view(-1, 32, 32)
-    for i in range(row_no):
-        for j in range(2):
-            if(j == 0):
-                grid[i*2+j].imshow(np.transpose(plot_input[i], (1, 2, 0)), interpolation="nearest")
-            if(j == 1):
-                grid[i*2+j].imshow(np.transpose(actual_output[i].detach().cpu().numpy(), (0, 1)), interpolation="nearest")
+def dataset_to_tensor(dataset):
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, num_workers=1)
+    output = None
+    for batch_idx, (img, target) in enumerate(dataloader):
+        if output is None:
+            output = img.view(-1, 3072)
+        else:
+            output = torch.cat((output, img.view(-1, 3072)))
+    return output
+
+def PCA_data(train_dataset, val_dataset):
+    train_data = dataset_to_tensor(train_dataset)
+    val_data = dataset_to_tensor(val_dataset)
+    pca.fit(train_data)
+    train_img_proj = pca.transform(train_data)
+    val_img_proj = pca.transform(val_data)
+    return torch.tensor(train_img_proj, dtype=torch.float32), torch.tensor(val_img_proj, dtype = torch.float32)
 
 def make_loader(args):
     data_transforms = data_transformations.__dict__[args.data_transforms]
     train_dataset = datasets.__dict__[args.dataset](is_train = True, supervised = True, data_transforms = data_transforms)
     val_dataset = datasets.__dict__[args.dataset](is_train = False, data_transforms = data_transforms)
+    train_dataset, val_dataset = PCA_data(train_dataset, val_dataset)
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=1)
     val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=1)
     return train_loader, val_loader
 
+def plot_grid(fig, plot_input, actual_output, row_no):
+    grid = ImageGrid(fig, 121, nrows_ncols=(row_no, 2), axes_pad=0.05, label_mode="1")
+    for i in range(row_no):
+        for j in range(2):
+            if(j == 0):
+                grid[i*2+j].imshow(np.transpose(plot_input[i], (1, 2, 0)), interpolation="nearest")
+            if(j == 1):
+                grid[i*2+j].imshow(np.transpose(actual_output[i], (1, 2, 0)), interpolation="nearest")
+
+def plot_images(data, output, epoch):
+    start_time = time.time()
+    mini_batch_size = 5
+    plot_input = data[0:5, :]
+    plot_output = output[0:5, :]
+    plot_input = np.clip(pca.inverse_transform(plot_input), 0, 0.999).reshape(-1, 3, 32, 32)
+    plot_output = np.clip(pca.inverse_transform(plot_output), 0, 0.999).reshape(-1, 3, 32, 32)
+    F = plt.figure(1, (30, 60))
+    F.subplots_adjust(left=0.05, right=0.95)
+    plot_grid(F, plot_input, plot_output, mini_batch_size)
+    images_folder = "images/" + current_time
+    if not os.path.isdir(images_folder):
+        os.mkdir(images_folder)
+    plt.savefig(images_folder + '/' + args.label + "_" +  str(epoch) + ".jpg")
+    plt.show()
+    end_time = time.time()
+    print('saving time:', round(end_time-start_time, 2))
+
+def validation(model, val_loader, epoch):
+    with torch.no_grad():
+        model.eval()
+        validation_loss = 0
+        for batch_idx, data in enumerate(val_loader):
+            data = data.to(device)
+            output, mu, logvar = model(data)
+            validation_loss += loss_function(output, data, mu, logvar).item() # sum up batch loss
+            if epoch % args.model_save_interval == 0 and batch_idx == 0:
+                plot_images(data, output, epoch)
+    validation_loss /= len(val_loader.dataset)
+    return validation_loss, len(val_loader.dataset)
+
 def train_unsupervised(model, epoch, train_loader, optimizer):
     model.train()
     training_loss = 0
-    for batch_idx, (data, _) in enumerate(train_loader):
+    for batch_idx, data in enumerate(train_loader):
         data = data.to(device)
         # ===================forward=====================
         optimizer.zero_grad()
@@ -90,40 +147,15 @@ def train_unsupervised(model, epoch, train_loader, optimizer):
     training_loss /= len(train_loader.dataset)
     return training_loss, len(train_loader.dataset)
 
-def validation(model, val_loader, epoch):
-    with torch.no_grad():
-        model.eval()
-        validation_loss = 0
-        for batch_idx, (data, _) in enumerate(val_loader):
-            data = data.to(device)
-            output, mu, logvar = model(data)
-            validation_loss += loss_function(output, data, mu, logvar).item() # sum up batch loss
-            if epoch % args.model_save_interval == 0 and batch_idx == 0:
-                start_time = time.time()
-                mini_batch_size = 5
-                plot_input = data[0:5, :]
-                plot_output = output[0:5, :]
-                F = plt.figure(1, (30, 60))
-                F.subplots_adjust(left=0.05, right=0.95)
-                plot_grid(F, plot_input, plot_output, mini_batch_size)
-                images_folder = "images/" + current_time
-                if not os.path.isdir(images_folder):
-                    os.mkdir(images_folder)
-                plt.savefig(images_folder + '/' + args.label + "_" +  str(epoch) + ".jpg")
-                plt.show()
-                end_time = time.time()
-                print('saving time:', round(end_time-start_time, 2))
-    validation_loss /= len(val_loader.dataset)
-    return validation_loss, len(val_loader.dataset)
-
 def main(args):
     torch.manual_seed(args.seed)
+    train_loader, val_loader = make_loader(args)
     nclasses = datasets.__dict__[args.dataset].nclasses
-    model = models.__dict__[args.arch](nclasses = nclasses)
+    model = models.__dict__[args.arch](nclasses = nclasses, flatten_img_size=pca.components_.shape[0])
+    print('model summary:', summary(model, (3, pca.components_.shape[0], pca.components_.shape[0])))
     model = torch.nn.DataParallel(model).to(device)
     model.to(device)
     optimizer = torch.optim.Adam(model.parameters(),lr=args.lr,)
-    train_loader, val_loader = make_loader(args)
     report = PrettyTable(['Epoch #', 'Train loss', 'Train Total', 'Val loss', 'Val Total', 'Time(secs)'])
     best_validation_loss = 0
     for epoch in range(1, args.epochs + 1):
@@ -170,7 +202,7 @@ if __name__ == '__main__':
                         help="Do you want to save models for this run or not. (y) for saving the model")
     parser.add_argument('--model_save_interval', type=int, default=50, metavar='D',
                         help="No of epochs after which you want to save models for this run")
-    parser.add_argument('--label', type=str, metavar='D', help="label for this run")
+    parser.add_argument('--label', type=str, metavar='D', required = True, help="label for this run")
     # Model structure
     parser.add_argument('--arch', '-a', metavar='ARCH', default='vae_net',
                         choices=model_names,
@@ -205,7 +237,7 @@ if __name__ == '__main__':
 
     if args.hyper_param == 'LR':
         tuning_report = PrettyTable(['LR', 'Best Val loss'])
-        lrs = [10.0**j for j in range(-6,1,1)]
+        lrs = [10.0**j for j in range(-6,-1,1)]
         for lr in lrs:
             args.lr = lr
             args.label = args.hyper_param + ':' + str(lr)
--- models/vae.py
+++ models/vae.py
@@ -4,25 +4,50 @@ import torch.nn.functional as F
 import pdb
 
 class VAE(nn.Module):
-    def __init__(self, d):
+    def __init__(self, d, flatten_img_size):
         super().__init__()
         self.d = d
-
+        self.input_size = flatten_img_size
+        self.inter_dim = 2048
         self.encoder = nn.Sequential(
-            nn.Linear(1024, 512),
+            nn.Linear(flatten_img_size, self.inter_dim),
+            nn.BatchNorm1d(self.inter_dim),
             nn.ReLU(),
-	        nn.Linear(512, 256),
+            nn.Dropout2d(p=0.2),
+	        nn.Linear(self.inter_dim, self.inter_dim),
+            nn.BatchNorm1d(self.inter_dim),
             nn.ReLU(),
-            nn.Linear(256, d * 2)
+            nn.Dropout2d(p=0.2),
+            nn.Linear(self.inter_dim, self.inter_dim),
+            nn.BatchNorm1d(self.inter_dim),
+            nn.ReLU(),
+            nn.Dropout2d(p=0.2),
+            nn.Linear(self.inter_dim, self.inter_dim),
+            nn.BatchNorm1d(self.inter_dim),
+            nn.ReLU(),
+            nn.Dropout2d(p=0.2),
+            nn.Linear(self.inter_dim, d * 2)
         )
 
         self.decoder = nn.Sequential(
-            nn.Linear(d, 256),
+            nn.Linear(d, self.inter_dim),
+            nn.BatchNorm1d(self.inter_dim),
             nn.ReLU(),
-            nn.Linear(256, 512),
+            nn.Dropout2d(p=0.2),
+            nn.Linear(self.inter_dim, self.inter_dim),
+            nn.BatchNorm1d(self.inter_dim),
             nn.ReLU(),
-            nn.Linear(512, 1024),
-            nn.Sigmoid(),
+            nn.Dropout2d(p=0.2),
+            nn.Linear(self.inter_dim, self.inter_dim),
+            nn.BatchNorm1d(self.inter_dim),
+            nn.ReLU(),
+            nn.Dropout2d(p=0.2),
+            nn.Linear(self.inter_dim, self.inter_dim),
+            nn.BatchNorm1d(self.inter_dim),
+            nn.ReLU(),
+            nn.Dropout2d(p=0.2),
+            nn.Linear(self.inter_dim, flatten_img_size),
+            nn.Sigmoid()
         )
 
     def reparameterise(self, mu, logvar):
@@ -34,12 +59,12 @@ class VAE(nn.Module):
             return mu
 
     def forward(self, x):
-        mu_logvar = self.encoder(x.view(-1, 1024)).view(-1, 2, self.d)
+        mu_logvar = self.encoder(x.view(-1, self.input_size)).view(-1, 2, self.d)
         mu = mu_logvar[:, 0, :]
         logvar = mu_logvar[:, 1, :]
         z = self.reparameterise(mu, logvar)
         return self.decoder(z), mu, logvar
 
 def vae_net(**kwargs):
-    model = VAE(20)
+    model = VAE(512, kwargs['flatten_img_size'])
     return model
